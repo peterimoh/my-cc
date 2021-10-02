f313b585f420594ef30a6f67ecc49c212192299b
peterimoh <peterimo2@gmail.com>
2021-10-02 03:58:34 +0100

    gloal

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar'
+import './globalStyle.scss'
+
+
+function App() {
+  return (
+    <div id="App-container">
+     <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Navbar = () => {
+    return (
+        <div>
+            i am navbar
+        </div>
+    )
+}
+
+export default Navbar
